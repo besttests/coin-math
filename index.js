5605599741ce12ce17a2bb79bfd25a7429bf708b
ypocat <juraj.vitko@gmail.com>
2014-04-17 19:20:36 +0300

    update

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ exports.floatToFixed = function(value, checks, precise) {
     if(checks) {
 
         if(isNaN(value))
-            _e('Not a number: %d', value);
+            throw new Exception('Not a number: %d', value);
         if(value < 0)
-            _e('Value cannot be negative: %d', value);
+            throw new Exception('Value cannot be negative: %d', value);
         if(value > 90071992.54740992)
-            _e('Value is too large: %d', value);
+            throw new Exception('Value is too large: %d', value);
 
         if(precise) {
             var rem = value - Math.floor(value);
             if(rem !== 0 && rem < 1e-8)
-                _e('Value would lose precision: %d', value);
+                throw new Exception('Value would lose precision: %d', value);
         }
     }
 
